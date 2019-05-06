7b4d661e2a46c71df06839ea6dc5a24727e66f62
Katharina Bitzan <katharinabitzan@gmail.com>
2019-05-06 19:23:21 +0100

    add gems

--- app.rb
+++ app.rb
@@ -3,6 +3,8 @@ require( 'sinatra/contrib/all' ) if development?
 require_relative('controllers/members_controller')
 require_relative('controllers/tours_controller')
 require_relative('controllers/bookings_controller')
+config.assets.initialize_on_precompile = false
+
 
 get '/' do
   @upcoming_tours_next_30_days = Tour.upcoming_tours_next_30_days
