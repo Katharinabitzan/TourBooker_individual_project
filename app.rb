4809b5aa3ed2640ad412751b301ad32e60516f48
Katharina Bitzan <katharinabitzan@gmail.com>
2019-05-06 19:33:40 +0100

    remove conif line

--- app.rb
+++ app.rb
@@ -3,7 +3,6 @@ require( 'sinatra/contrib/all' ) if development?
 require_relative('controllers/members_controller')
 require_relative('controllers/tours_controller')
 require_relative('controllers/bookings_controller')
-config.assets.initialize_on_precompile = false
 
 
 get '/' do
